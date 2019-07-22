a19748498044d8345abf67d3e91d816911552868
Elízabeth Guardado <52927171+eguardadoav@users.noreply.github.com>
2019-07-22 15:36:17 -0600

    Update Ejercicio1.java

--- src/TAREA1/Ejercicio1.java
+++ src/TAREA1/Ejercicio1.java
@@ -5,23 +5,23 @@
  */
 package TAREA1;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+
 
 /**
  *
  * @author elizabethguardado
  */
 public class Ejercicio1 {
+    Scanner l = new Scanner(System.in);
     int numero1;
         int numero2;
         int resultado;
-        BufferedReader num;
-        num = new BufferedReader (new InputStreamReader (System.in));
+        
+        
        System.out.println("Introduce el primer numero: ");
-       numero1 = Integer.parseInt(num.readLine());
+       numero1 = l.nextInt();
        System.out.println("Introduce el segundo numero: ");
-       numero2 = Integer.parseInt(num.readLine());
+       numero2 = l.nexInt();
        resultado = numero1 * numero2;
        System.out.println("La multiplicacion es " + numero1 + " * " + numero2 + " = " + resultado );
     
